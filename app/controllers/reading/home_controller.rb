4573520bd936301c9391028b7a227f0ef225fb57
Jitang Zheng <jitang.zheng@gmail.com>
2016-11-21 08:58:44 -0800

    home

---
+++ app/controllers/reading/home_controller.rb
@@ -0,0 +1,8 @@
+require_dependency "reading/application_controller"
+
+module Reading
+  class HomeController < ApplicationController
+    def index
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 Reading::Engine.routes.draw do
+  root to: 'home#index'
+  resources :notes
+
 end
