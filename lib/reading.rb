6d510ff90ba856624399ddf95593f92a1d87dcf1
Jitang Zheng <jitang.zheng@gmail.com>
2016-11-22 15:57:23 -0800

    dashboard nav-bar

--- lib/reading.rb
+++ lib/reading.rb
@@ -1,5 +1,14 @@
-require "reading/engine"
+require 'reading/engine'
 
 module Reading
-  # Your code goes here...
+  def Reading.dashboard(user)
+    {
+        label: 'reading.home.dashboard.title',
+        links: [
+            {label: '111', href: 'root_path'},
+            {label: '222', href: 'root_path'},
+            {label: '333', href: 'root_path'},
+        ]
+    }
+  end
 end
--- lib/reading/version.rb
+++ lib/reading/version.rb
@@ -1,3 +1,3 @@
 module Reading
-  VERSION = '0.1.0'
+  VERSION = '2016.11.22'
 end
