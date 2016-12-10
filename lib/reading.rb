23758f3d72c765d04dc652a880276b4522cafe23
Jitang Zheng <jitang.zheng@gmail.com>
2016-12-10 08:13:24 -0800

    sitemap

--- lib/reading.rb
+++ lib/reading.rb
@@ -1,7 +1,7 @@
 require 'reading/engine'
 
 module Reading
-  def Reading.dashboard(user)
+  def self.dashboard(user)
     links = [
         {label: 'reading.notes.index.title', href: 'notes_path'},
         {label: 'reading.favorites.index.title', href: 'favorites_path'},
@@ -15,5 +15,23 @@ module Reading
         label: 'reading.dashboard.title',
         links: links,
     }
+  end
+
+  def self.sitemap
+    app = Reading::Engine.routes.url_helpers
+    links = []
+    Setting.languages.each do |l|
+
+      links << {url: app.hot_notes_path(locale:l)}
+      links << {url: app.latest_notes_path(locale:l)}
+
+      links << {url: app.hot_books_path(locale:l)}
+      links += Book.select(:id, :updated_at).map do |i|
+        {url:book_path(i), lastmod:i.updated_at}
+      end
+
+      links << {url: app.root_path(locale:l)}
+    end
+    links
   end
 end
